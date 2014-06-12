0fe2025987029e488f31e41b3ec4beeeb55152e3
JustDNA <dhiwakar.nitt@gmail.com>
2014-06-12 11:08:10 +0530

    new commit

--- Assn1.py
+++ Assn1.py
@@ -1,30 +1,38 @@
-def add(a,b):
-    print a+b
-    
-def sub(a,b):
-    print a-b
-    
-def mul(a,b):
-    print a*b
-    
-def div(a,b):
-    print a/b
-    
-running = True
-while running:
-    choice = int(input("Enter choice\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Quit"))
-    
-    if(choice!=5):
-        a = float(input("Enter value for a"))
-        b = float(input("Enter value for b"))
+__author__ = 'dhiwakar'
 
+def add(a,b):
+    print (a+b)
+
+def sub(a,b):
+    print (a-b)
+
+def mul(a,b):
+    print (a*b)
+
+def div(a,b):
+    print (a/b)
+
+choice = int(input("Enter choice\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Quit\n"))
+
+if(choice < 5):
+    running = True
+else:
+    running = False
+
+while running:
+    
+    a = float(input("Enter value for a"))
+    b = float(input("Enter value for b"))
+    
     if(choice == 1):
         add(a,b)
     if(choice == 2):
-            sub(a,b)
+        sub(a,b)
     if(choice == 3):
-            mul(a,b)
+        mul(a,b)
     if(choice == 4):
-            div(a,b)
-    if(choice > 4):
+        div(a,b)
+    
+    choice = int(input("Enter choice\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Quit\n"))
+    if(choice >= 4):
         running = False
